30737c9eade3b7b820aaef5659071903a00cca5b
Liam Skirrow <skirrowliam@gmail.com>
2024-01-09 22:02:44 +0000

    Created handy shell script to keep synth source files up to date

---
+++ copy-files-to-vivado.sh
@@ -0,0 +1,4 @@
+cp src/wrapper.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/wrapper.v
+cp riscv-cpu/src/top.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/top.v
+cp riscv-cpu/src/alu.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/alu.v
+cp riscv-cpu/src/registerfile.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/registerfile
