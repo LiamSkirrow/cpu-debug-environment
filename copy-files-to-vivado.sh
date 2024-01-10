3114ff7eeef98425c08c4bb6f7170be42b439484
Liam Skirrow <skirrowliam@gmail.com>
2024-01-10 21:33:37 +0000

    bump rv, fix copy script typo

--- copy-files-to-vivado.sh
+++ copy-files-to-vivado.sh
@@ -1,4 +1,5 @@
 cp src/wrapper.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/wrapper.v
 cp riscv-cpu/src/top.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/top.v
 cp riscv-cpu/src/alu.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/alu.v
-cp riscv-cpu/src/registerfile.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/registerfile
+cp riscv-cpu/src/registerfile.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/registerfile.v
+cp riscv-cpu/src/defines.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/defines.v
