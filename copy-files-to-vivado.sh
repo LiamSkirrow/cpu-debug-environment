e4af33895ffd34349c053541027dd256841ed1b2
Liam Skirrow <skirrowliam@gmail.com>
2024-01-10 21:45:06 +0000

    Modified copy script

--- copy-files-to-vivado.sh
+++ copy-files-to-vivado.sh
@@ -2,4 +2,4 @@ cp src/wrapper.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imp
 cp riscv-cpu/src/top.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/top.v
 cp riscv-cpu/src/alu.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/alu.v
 cp riscv-cpu/src/registerfile.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/registerfile.v
-cp riscv-cpu/src/defines.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/defines.v
+cp riscv-cpu/src/defines.v ../../VivadoProjects/riscv-synth/riscv-synth.srcs/sources_1/imports/src/src/defines.v
